ed59ba1600103d6b10bfdae7756150ceb8e001ba
Your Name <contact@johnloeber.com>
2026-02-15 14:56:10 -0800

    Delete old incantation of glupper

--- archivebox_backend/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-ArchiveBox backend package for Glupper.
-
-This package contains the worker and utilities for processing archive jobs
-from the SQS queue and storing archives in S3.
-"""
-
-__version__ = "0.1.0"
-
